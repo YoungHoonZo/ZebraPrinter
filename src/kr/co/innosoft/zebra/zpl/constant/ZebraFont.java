a55d516d980114a4322b88b41a47ac4221a44fd5
Gizmo80 <asura808080@gmail.com>
2020-12-17 10:24:33 +0900

    폰트추가(?) pom.xml java doc 추가 설정.

--- src/kr/co/innosoft/zebra/zpl/constant/ZebraFont.java
+++ src/kr/co/innosoft/zebra/zpl/constant/ZebraFont.java
@@ -2,7 +2,7 @@ package kr.co.innosoft.zebra.zpl.constant;
 
 public enum ZebraFont {
 
-	ZEBRA_ZERO("0"), ZEBRA_A("A"), ZEBRA_B("B"), ZEBRA_C("C"), ZEBRA_D("D"), ZEBRA_F("F"), ZEBRA_G("G");
+	ZEBRA_ZERO("0"), ZEBRA_A("A"), ZEBRA_B("B"), ZEBRA_C("C"), ZEBRA_D("D"), ZEBRA_F("F"), ZEBRA_G("G"), ZEBRA_ONE("1"), ZEBRA_K("K");
 
 	String letter;
 
